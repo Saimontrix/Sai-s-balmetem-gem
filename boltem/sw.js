768df19da335eb823bbb46032d6bc2138986874f
Saimontrix <sabertemporary@gmail.com>
2025-02-27 12:52:31 +0400

    first upload

---
+++ boltem/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'maze-game-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+}); 
